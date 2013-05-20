b9a61ad2806c6daf9def8f95fdb035972aadaf32
Ian Morgan <ian@ruby-code.com>
2013-05-20 00:57:55 -0400

    Revert "Don't try ltc pools"
    This reverts commit 746c2dec3ff5cc1e0f38631b824ae324714ccbde.

--- src/net.cpp
+++ src/net.cpp
@@ -1149,7 +1149,9 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"rubitpool.org"},
+    {"rubitpool.org", "dnsseed.rubitpool.org"},
+    {"bytesized-vps.com", "dnsseed.bytesized-vps.com"},
+    {"xurious.com", "dnsseed.ltc.xurious.com"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
