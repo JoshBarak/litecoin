5d39b741fa0c36d81da769d986d6c6c9a5e066b7
Ian Morgan <ian@ruby-code.com>
2013-05-19 20:53:25 -0400

    Make rub it

--- src/irc.cpp
+++ src/irc.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
-// Copyright (c) 2011-2012 Litecoin Developers
+// Copyright (c) 2011-2012 rubit Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #litecoinTEST3\r");
-            Send(hSocket, "WHO #litecoinTEST3\r");
+            Send(hSocket, "JOIN #rubitTEST3\r");
+            Send(hSocket, "WHO #rubitTEST3\r");
         } else {
-            // randomly join #litecoin00-#litecoin99
+            // randomly join #rubit00-#rubit99
             int channel_number = GetRandInt(100);
-            channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #litecoin%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #litecoin%02d\r", channel_number).c_str());
+            channel_number = 0; // rubit: for now, just use one channel
+            Send(hSocket, strprintf("JOIN #rubit%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #rubit%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
